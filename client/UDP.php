1245483fa1a4c99b8bbbaafe46c81c585f5ee6c6
aaa <834320733@qq.com>
2022-04-06 18:56:55 +0800

    UDP

--- client/UDP.php
+++ client/UDP.php
@@ -7,7 +7,7 @@
  */
 
 go(function(){
-    $client = new \Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
+    $client = new \Swoole\Coroutine\Client(SWOOLE_SOCK_UDP);
     if (!$client->connect('127.0.0.1',9502, 0.5)) {
         echo "connect failed: Error:{$client->errCode}\n";
     }
