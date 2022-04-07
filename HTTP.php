d9f10e5e8b35437a0e2b58ad98a556ad30640f6c
aaa <834320733@qq.com>
2022-04-07 18:13:52 +0800

    HTTP配置

--- HTTP.php
+++ HTTP.php
@@ -24,7 +24,7 @@ class HTTP
     }
 
     public function onRequest($request, $response) {
-        var_dump($request, $response);
+        var_dump($request['get'], $response['post']);
 
 
         $response->header('Content-Type', 'text/html; charset=utf-8');
