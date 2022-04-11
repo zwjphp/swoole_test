3eb63cccbe6e6a1b45234378502df67ec60bb933
aaa <834320733@qq.com>
2022-04-11 19:44:20 +0800

    redis

--- Redis.php
+++ Redis.php
@@ -1,24 +1,18 @@
 <?php
-use Swoole\Runtime;
-use Swoole\Coroutine;
+use Swoole\Coroutine\Http\Server;
 use function Swoole\Coroutine\run;
 
 class re {
-    public function index() {
-      Swoole\Runtime::enableCoroutine()
-
-      Co\run(function() {
-        for ($c = 100; $c--;) {
-            go(function () {//创建100个协程
-                $redis = new Redis();
-                $redis->connect('127.0.0.1', 6379);//此处产生协程调度，cpu切到下一个协程，不会阻塞进程
-                $redis->get('aaa');//此处产生协程调度，cpu切到下一个协程，不会阻塞进程
-                sleep(5);
-            });
-        }
+  public function index() {
+    run(function () {
+      $redis = new Redis();
+      $redis->connect('127.0.0.1', 6379);
+      var_dump($redis->get('key'));
+      sleep(5);
     });
-    }
-
-
+  }
 
 }
+
+$re = new re();
+$re->index();
