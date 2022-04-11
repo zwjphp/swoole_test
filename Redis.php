3c5c1df890fa844c63146e6b91ad874ad346588a
aaa <834320733@qq.com>
2022-04-11 20:54:22 +0800

    redis

--- Redis.php
+++ Redis.php
@@ -7,7 +7,7 @@ class re {
     run(function () {
       $redis = new Redis();
       $redis->connect('127.0.0.1', 6379);
-      var_dump($redis->get('key'));
+      var_dump($redis->get('aaa'));
       sleep(5);
     });
   }
